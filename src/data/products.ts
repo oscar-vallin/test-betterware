1253db194165690ec9fcd47ea49367f9705a15bc
oscarvallin <oscar.hernandez@ambarseguros.mx>
2024-05-27 20:50:16 -0600

    Added some products to display on the Home page

---
+++ src/data/products.ts
@@ -0,0 +1,40 @@
+import { CartProduct } from "../redux/types/state";
+
+export const listOfProducts: CartProduct [] = [
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct1.jpeg?alt=media&token=7ac4afb5-c699-45a7-aa7f-02865464657c',
+    name: 'product 1',
+    description: 'good product 1',
+    price: '10',
+  },
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct2.jpeg?alt=media&token=0ee61d20-b2d3-41e9-ba55-41512aee7b89',
+    name: 'product 2',
+    description: 'good product 2',
+    price: '20',
+  },
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct3.jpg?alt=media&token=e0320db0-fa04-43cc-b8dd-9097991f3834',
+    name: 'product 3',
+    description: 'good product 3',
+    price: '30',
+  },
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct4.jpeg?alt=media&token=367c794b-7c2e-4a11-9391-512c09ce1e54',
+    name: 'product 4',
+    description: 'good product 4',
+    price: '40',
+  },
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct5.jpeg?alt=media&token=d48190c2-0e52-46cd-b0df-3bf7cfff9bb2',
+    name: 'product 5',
+    description: 'good product 5',
+    price: '50',
+  },
+  { 
+    img: 'https://firebasestorage.googleapis.com/v0/b/venta-de-coches-gabriel.appspot.com/o/products%2Fproduct6.jpeg?alt=media&token=ca62fbc2-43c6-4fd6-9064-a73f53f4011b',
+    name: 'product 6',
+    description: 'good product 6',
+    price: '60',
+  },
+]
