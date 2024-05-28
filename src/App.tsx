1c323882b6d0d2422d808e510326a90d4821b200
oscarvallin <oscar.hernandez@ambarseguros.mx>
2024-05-27 20:41:55 -0600

    Added bootstrap import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
+
 import './App.css';
 import { RouteController } from './routes/Routes';
 
 function App() {
   return <RouteController />
-}
+};
 
 export default App;
