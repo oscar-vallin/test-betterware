5bdf9e1a80b4bcecba850d7e223c6ccf7f69d67a
oscarvallin <oscar.hernandez@ambarseguros.mx>
2024-05-28 19:04:56 -0600

    Removed function

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { AppState } from './types/state';
 import cartProductReducer from './cartProductReducer';
 
 const rootReducer = combineReducers<any>({
